a22982115ebfc446cf82c0f5b2851173ae0d23d9
Ahmed Naeem <ahmednaeem1012@gmail.com>
2023-11-13 18:38:09 +0500

    changes for beta version testing

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -68,7 +68,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
         backgroundColor: Colors.white,
         body: Padding(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.all(40),
           child:
               Center(child: Platform.isIOS ? applePayButton : googlePayButton),
         ));
