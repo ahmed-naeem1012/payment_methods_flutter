290834e1a34e90bacb85a1bc646c60b8008fcf1f
Ahmed Naeem <ahmednaeem1012@gmail.com>
2023-11-13 18:36:11 +0500

    paymentmethods

---
+++ lib/homepage.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_constructors, unused_import
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:pay/pay.dart';
+import 'package:payment_methods_flutter/payment_config.dart';
+
+import 'payment_config.dart' as payment_configurations;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String os = Platform.operatingSystem;
+
+  var applePayButton = ApplePayButton(
+    paymentConfiguration: PaymentConfiguration.fromJsonString(defaultApplePay),
+    paymentItems: const [
+      PaymentItem(
+        label: 'Item A',
+        amount: '0.01',
+        status: PaymentItemStatus.final_price,
+      ),
+      PaymentItem(
+        label: 'Item B',
+        amount: '0.01',
+        status: PaymentItemStatus.final_price,
+      ),
+      PaymentItem(
+        label: 'Total',
+        amount: '0.02',
+        status: PaymentItemStatus.final_price,
+      )
+    ],
+    style: ApplePayButtonStyle.black,
+    width: double.infinity,
+    height: 50,
+    type: ApplePayButtonType.buy,
+    margin: const EdgeInsets.only(top: 15.0),
+    onPaymentResult: (result) => debugPrint('Payment Result $result'),
+    loadingIndicator: const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+  var googlePayButton = GooglePayButton(
+    paymentConfiguration: PaymentConfiguration.fromJsonString(defaultGooglePay),
+    paymentItems: const [
+      PaymentItem(
+        label: 'Total',
+        amount: '0.01',
+        status: PaymentItemStatus.final_price,
+      )
+    ],
+    type: GooglePayButtonType.pay,
+    margin: const EdgeInsets.only(top: 15.0),
+    onPaymentResult: (result) => debugPrint('Payment Result $result'),
+    loadingIndicator: const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child:
+              Center(child: Platform.isIOS ? applePayButton : googlePayButton),
+        ));
+  }
+}
