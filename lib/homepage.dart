697256487f8852f7bfde17d7d49489ffefffc8a8
Ahmed Naeem <ahmednaeem1012@gmail.com>
2023-11-13 18:49:54 +0500

    green added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -66,7 +66,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.green,
         body: Padding(
           padding: EdgeInsets.all(40),
           child:
